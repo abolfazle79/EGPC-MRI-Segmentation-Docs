0dc51f6bad64111959a3940ccda823a4d1ed310b
abolfazle79 <abolfazle.abolghasemi79@gmail.com>
2025-06-09 15:48:59 +0330

    docs change config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://abolfazle79.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/EGPC-MRI-Segmentation-Docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "abolfazle79", // Usually your GitHub org/user name.
+  projectName: "/EGPC-MRI-Segmentation-Docs/", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
